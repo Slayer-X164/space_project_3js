a8352cd194956fa63dd4061b7226cc5f83f4df1a
Slayer-X164 <siddheshghag150@gmail.com>
2025-04-18 00:27:44 +0530

    add

--- src/main.js
+++ src/main.js
@@ -186,6 +186,7 @@ controls.autoRotate = false
 
 const renderer = new THREE.WebGLRenderer({canvas,antialias:true})
 renderer.setSize(window.innerWidth,window.innerHeight)
+
 renderer.setPixelRatio(Math.min(window.devicePixelRatio,2))
 
 window.addEventListener('resize',()=>{
